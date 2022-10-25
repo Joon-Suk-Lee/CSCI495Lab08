6ecb918c0270f251f949c205771f32b6b493d145
jlee <dolomite00@gmail.com>
2022-10-24 23:46:55 -0400

    Lab08 Due by 11:59 PM on 10/25/2022

--- CSCI495Lab08/main.swift
+++ CSCI495Lab08/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+func calculateSubTotal(_ list: Array<Array<Int>>) ->Array<Int> {
+    var returnArray: [Int] = []
+
+    // fill out this part
+    //
+    // do not change existing lines (except the header block)
+    
+    return returnArray
+}
+
+
+
+print(calculateSubTotal([[11,11,22], [22], [], [1,2,3]]))
 
